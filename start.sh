bb9806e6d87285b71ea23932b95e2ffcc35624d1
obakhuis-star <o.bakhuis@gmail.com>
2026-05-10 13:19:50 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
     mkdir -p /root/.letta/channels/telegram
-    cat > /root/.letta/channels/telegram/accounts.json << 'EOF'
-    {"accounts":[{"channel":"telegram","accountId":"587c43d9-6ebd-47a2-80b6-dbed
-    84827f04","displayName":"@Orlando_opa_bot","enabled":true,"token":"855131247
-    4:AAGK2nxayTfnFKChMup0DE6pbueO_ERyv1o","dmPolicy":"pairing","allowedUsers":[
-    ],"transcribeVoice":false,"binding":{"agentId":null,"conversationId":null}}]
-    }
-    EOF
-    letta server --env-name cloud --channels telegram
+    printf '%s\n' '{"accounts":[{"channel":"telegram","accountId":"587c43d9-6ebd
+    -47a2-80b6-dbed84827f04","displayName":"@Orlando_opa_bot","enabled":true,"to
+    ken":"8551312474:AAGK2nxayTfnFKChMup0DE6pbueO_ERyv1o","dmPolicy":"pairing","
+    allowedUsers":[],"transcribeVoice":false,"binding":{"agentId":null,"conversa
+    tionId":null}}]}' > /root/.letta/channels/telegram/accounts.json
+    letta server --env-name "$ENV_NAME" --channels telegram
