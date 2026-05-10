9eb4113009006fde8c43fbd3e85a06029716d257
obakhuis-star <o.bakhuis@gmail.com>
2026-05-10 15:39:44 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
     mkdir -p /root/.letta/channels/telegram
     base64 -d > /root/.letta/channels/telegram/accounts.json << 'EOF'
-    eyJhY2NvdW50cyI6W3siY2hhbm5lbCI6InRlbGVncmFtIiwiYWNjb3VudElk
-    IjoiNTg3YzQzZDktNmViZC00N2EyLTgwYjYtZGJlZDg0ODI3ZjA0IiwiZGlz
-    cGxheU5hbWUiOiJAT3JsYW5kb19vcGFfYm90IiwiZW5hYmxlZCI6dHJ1ZSwi
-    dG9rZW4iOiI4NTUxMzEyNDc0OkFBR0sybnhheVRmbkZLQ2hNdXAwREU2cGJ1
-    ZU9fRVJ5djFvIiwiZG1Qb2xpY3kiOiJwYWlyaW5nIiwiYWxsb3dlZFVzZXJz
-    IjpbXSwidHJhbnNjcmliZVZvaWNlIjpmYWxzZSwiYmluZGluZyI6eyJhZ2Vu
-    dElkIjpudWxsLCJjb252ZXJzYXRpb25JZCI6bnVsbH19XX0=
+    eyJhY2NvdW50cyI6W3siY2hhbm5lbCI6InRlbGVncmFtIiwi
+    YWNjb3VudElkIjoiNTg3YzQzZDktNmViZC00N2EyLTgwYjYt
+    ZGJlZDg0ODI3ZjA0IiwiZGlzcGxheU5hbWUiOiJAT3JsYW5k
+    b19vcGFfYm90IiwiZW5hYmxlZCI6dHJ1ZSwidG9rZW4iOiI4
+    NTUxMzEyNDc0OkFBR0sybnhheVRmbkZLQ2hNdXAwREU2cGJ1
+    ZU9fRVJ5djFvIiwiZG1Qb2xpY3kiOiJwYWlyaW5nIiwiYWxs
+    b3dlZFVzZXJzIjpbXSwidHJhbnNjcmliZVZvaWNlIjpmYWxz
+    ZSwiYmluZGluZyI6eyJhZ2VudElkIjpudWxsLCJjb252ZXJz
+    YXRpb25JZCI6bnVsbH19XX0=
     EOF
     letta server --env-name "$ENV_NAME" --channels telegram
